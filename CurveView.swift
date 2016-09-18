c433ff473313d24295e03b07674f1acded5f7888
agoettler <agoettler@gmail.com>
2016-09-18 16:15:18 -0500

    Completed layout of user interface

---
+++ CurveView.swift
@@ -0,0 +1,21 @@
+//
+//  CurveView.swift
+//  BezierCurveDemo
+//
+//  Created by Andrew Goettler on 9/18/16.
+//  Copyright © 2016 Andrew Goettler. All rights reserved.
+//
+
+import UIKit
+
+class CurveView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
