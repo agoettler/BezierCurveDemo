033c18ad0f2c195cc45cfa49dba14a0181e6b9c1
agoettler <agoettler@gmail.com>
2016-09-18 17:34:00 -0500

    Added code to draw a boundary line around the CurveView

--- CurveView.swift
+++ CurveView.swift
@@ -8,14 +8,39 @@
 
 import UIKit
 
-class CurveView: UIView {
-
+class CurveView: UIView
+{
+    // these may not be necessary
+    var topLeftCorner: CGPoint?
+    var topRightCorner: CGPoint?
+    var bottomRightCorner: CGPoint?
+    var bottomLeftCorner: CGPoint?
+    
+    // if the corner points are not necessary, this function may be removed
+    func setGeometry()
+    {
+        topLeftCorner = bounds.origin
+        topRightCorner = CGPoint(x: bounds.origin.x + bounds.width, y: bounds.origin.y)
+        bottomRightCorner = CGPoint(x: bounds.origin.x + bounds.width, y: bounds.origin.y + bounds.height)
+        bottomLeftCorner = CGPoint(x: bounds.origin.x, y: bounds.origin.y + bounds.height)
+    }
+    
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
     */
+    override func draw(_ rect: CGRect)
+    {
+        // Drawing code
+        
+        self.setGeometry()
+        
+        let viewBoundary: UIBezierPath = UIBezierPath(rect: bounds)
+        viewBoundary.lineWidth = 2
+        viewBoundary.lineCapStyle = CGLineCap.round
+        UIColor.black.setStroke()
+        viewBoundary.stroke()
+    }
+ 
 
 }
